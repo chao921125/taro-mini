59b06b20231b2461f7e1922ce9c2b6e6f9528582
hc precog <chao921125@sina.com>
2023-09-05 08:55:03 +0800

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,22 +6,8 @@ module.exports = {
 		node: true,
 		"vue/setup-compiler-macros": true,
 	},
-	extends: ["taro/vue3", "plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier", "@vue/typescript"],
+	extends: ["taro/vue3", "plugin:vue/vue3-essential", "eslint:recommended"],
 	parserOptions: {
 		parser: "@typescript-eslint/parser",
-	},
-	rules: {
-		"prettier/prettier": [
-			"error",
-			{
-				singleQuote: true,
-				semi: false,
-				trailingComma: "none",
-				arrowParens: "avoid",
-				printWidth: 100,
-			},
-		],
-		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
 	},
 };
